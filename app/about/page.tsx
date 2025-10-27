4974872e0a848cc6651e71cc1691c4aa31f3296d
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-10-27 23:21:34 +0530

    changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,53 @@
 export default function About() {
-  return <h1>About Us</h1>;
+  return <>
+    <div className="navbar">
+      <div className="navlogo">
+        <img className="navlogo logo" src="https://chat.google.com/u/0/api/get_attachment_url?url_type=FIFE_URL&content_type=image%2Fpng&attachment_token=AOo0EEUDqakedWrBI44qDGrIcKHZ3FdoqG9HZdk9yBhlQ9l8ZVRGBsqL88Jh6SUWZZt5CbOVzY%2B01fICZCGJjBo9UOKimA8lre%2FhBSjXDcV41NcxyHJOqCUovcCzHXuu8Ub4BiV7Bfv0GNbmtPxA5EPUoe0aBy7eb5Oo3mDVx0xkuS%2FAWNy7W43XrLYH71Zu7CrruogiNNjAWOnzEQbftTGtukXWlMIEVHT2j%2B0AiV8wsqzm78RbTTfC0mrEDQVD9ChEyKxvTqrm9mKqGEXud%2ByDGn87cwCPguP%2ByU3rwT6jghrJwIt78g4KTp%2BKYPhj2BY%2Fjz7AO7PzllLqevDOMH6hSVX7%2Brg3lMw5oWyIhxaVI93NYV2RkeazFW6NAVknScGXz4YG2uhy47CLCUHze70SSHL9pxEOlAFLhm19Kb7u7Y%2FaO9Je4HlcnFe25L4asdfLfBaBFHXlEFBvkVt3lyusV4EAXFNGt%2Fu7aYfFrq6neo0732T7dy3td1hRCUFs4pZs5MlcbjRMXVzKUIIpQlcCfxW0WXQ7ynt6PQzFUy0LwjmMf2gRotBmCpfNpSQEDW97NQ%3D%3D&sz=w512"/>
+        <nav className="navlogo businessName"> Sol-Air
+        </nav>
+      </div>
+      <ul>
+        <li><a href="https://codepen.io/G8E-Lim/full/BaEvWPx">Home</a></li>
+        <li><a href="https://codepen.io/G8E-Lim/full/BaEvpKo">Products</a></li>
+        <li><a href="https://codepen.io/G8E-Lim/full/BaEvWwY">About Us</a></li>
+        <li><a href="https://codepen.io/G8E-Lim/full/qBwaQaM">Contact Us</a></li>
+      </ul>
+    </div>
+
+    <div className="flexbox_container">
+      <h1></h1>
+      <div className="flexbox">
+
+        <div className="boxmodel">
+          <div><img className="productpage_pic" src="https://cdn.discordapp.com/attachments/915804339283566682/1227936980856344596/image.png?ex=662a386e&is=6617c36e&hm=b74dec2eb6f272ab1232f8af6abb573c9a4e04db7918223a47ebe836e472cb64&" /></div>
+          <p id="productname">Jeri Austria</p>
+          <p id="productinfo">Leader/Hero Image maker</p>
+        </div>
+
+        <div className="boxmodel">
+          <div><img className="productpage_pic" src="https://cdn.discordapp.com/attachments/915804339283566682/1227937112196780052/image.png?ex=662a388d&is=6617c38d&hm=74f8427382abe565ef8fc0a432dbc636ac6dcc26ac63a76a396de9de61ac4cfa&" /></div>
+          <p id="productname">Teia Abad</p>
+          <p id="productinfo"> Product Page Maker</p>
+        </div>
+
+        <div className="boxmodel">
+          <div><img className="productpage_pic" src="https://cdn.discordapp.com/attachments/915804339283566682/1227937188550017124/image.png?ex=662a389f&is=6617c39f&hm=a084165703bc72cbc5ab75fba39673cc54b6e771a6187723d53ca761d6af373f&" /></div>
+          <p id="productname">Enzo Lim</p>
+          <p id="productinfo">Contact Us Page Maker</p>
+        </div>
+
+        <div className="boxmodel">
+          <div><img className="productpage_pic" src="https://cdn.discordapp.com/attachments/915804339283566682/1227937652947419266/image.png?ex=662a390e&is=6617c40e&hm=90ab389bca6088a124113abd939f5deffab3e68a88331dab6eb034abe611d298&" /></div>
+          <p id="productname">Matteo Hernandez</p>
+          <p id="productinfo">Home Page Maker</p>
+        </div>
+      </div>
+
+      <div className="boxmodel">
+        <div><img className="productpage_pic" src="https://cdn.discordapp.com/attachments/915804339283566682/1227937249107116042/image.png?ex=662a38ae&is=6617c3ae&hm=0bd92df2498df72047e52ac409a72710edee3f195be943d8b538d551ca2e12c2&" /></div>
+        <p id="productname">Luigi Mendoza</p>
+        <p id="productinfo">About Us Page Maker</p>
+      </div>
+    </div>
+  </>
 }
